55cf0553b5e2f949a8b37e0627c6da6e3c1b6b20
LukasT03 <tuchscherer.lukas@icloud.com>
2025-10-15 14:03:24 +0200

    Fixed localization

--- LTS Control/AboutView.swift
+++ LTS Control/AboutView.swift
@@ -275,7 +275,6 @@ struct AboutView: View {
                                     }
                                 }
                                 .foregroundColor(.secondary)
-
                                 Spacer()
                             }
                             .padding(.horizontal)
